0f2d443e4257a2916da8cf9047ec277882160b76
Beste <bestegursoy52@hotmail.com>
2024-11-26 21:16:29 +0300

    hello kitty selo<3

--- project/notification_service/notification_service/settings.py
+++ project/notification_service/notification_service/settings.py
@@ -32,14 +32,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
-    'django.contrib.auth',
+    #'django.contrib.admin',
+    #'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'src',
-    'notification_service',
     'channels',
 ]
 
--- project/notification_service/src/consumers.py
+++ project/notification_service/src/consumers.py
@@ -1,25 +1,25 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import async_to_sync
 import json
-#from project.notification_service import notification_service
+from .models import Notification
+from django.conf import settings
+settings.configure()
 
 class NotificationConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
         self.group_name = 'notification_service'
-        # Kullanıcıyı gruba ekle
         await self.channel_layer.group_add(
-            self.group_name,
+            self.group_name, 
             self.channel_name
+        
         )
-
-        # Bağlantıyı kabul et
         await self.accept()
-        await self.send(text_data=json.dumps({"message" : "Connected"}))
+        await self.send(text_data=json.dumps({"message": "Connected"}))
 
     async def disconnect(self, close_code):
-        # Kullanıcıyı gruptan çıkar
         await self.channel_layer.group_discard(
-            self.group_name,
+            self.group_name, 
             self.channel_name
         )
 
@@ -27,22 +27,49 @@ class NotificationConsumer(AsyncWebsocketConsumer):
         try:
             text_data_json = json.loads(text_data)
             message = text_data_json['message']
+            print("Mesaj alındı:", text_data_json)
 
             event = {
-                'type': 'notification_message',
-                'message': message
+                'user_name': 'admin',
+                'notification_type': 'notification_message',
+                'content': message,
+                'is_read': False,
             }
+            print("Mesaj alındı2:", text_data_json)
 
+            # Veritabanına bildirim kaydetme işlemi
+            await self.save_notification(
+                user_name=event['user_name'],
+                notification_type=event['notification_type'],
+                content=event['message'],
+                is_read=event['is_read'],
+            )
+            print("Mesaj alındı3:", text_data_json)
+
+            # Mesajı gruba gönder
             await self.channel_layer.group_send(self.group_name, event)
+            print("Mesaj alındı4:", text_data_json)
+
         except json.JSONDecodeError:
-            await self.send(text_data=json.dumps({"message" : "Invalid format. Must be JSON"}))
-        #gruptaki herkese mesajı gönderir
+            await self.send(text_data=json.dumps({"message": "Invalid format. Must be JSON"}))
+
+    # Veritabanı kaydetme işlemini asenkron hale getiren yardımcı fonksiyon
+    @async_to_sync
+    def save_notification(self, user_name, notification_type, content, is_read):
+        Notification.objects.create(
+            user_name=user_name,
+            notification_type=notification_type,
+            content=content,
+            is_read=is_read,
+        )
 
     # Bildirim alındığında çağrılır
     async def notification_message(self, event):
-        # Mesajı kullanıcıya gönder
+
         await self.send(text_data=json.dumps({
-            'type': event['type'],
-            'message': event['message'],
+            'user_name': event['user_name'],
+            'notification_type': event['notification_type'],
+            'content': event['message'],
+            'is_read': event['is_read'],
         }))
-        #olayları websocket istemcisine iletir
+
