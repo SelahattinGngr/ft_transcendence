12f44dbf682cf2a3dcc9b8e1af25ab6e474a035a
Beste <bestegursoy52@hotmail.com>
2024-12-05 23:49:46 +0300

    log_service başlandı

---
+++ project/log_service/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# PostgreSQL'i başlat
+/app/log_service/db.sh &
+
+# PostgreSQL'in başlatılmasını beklemek için biraz zaman tanı
+sleep 10
+
+# Auth servisini başlat
+cd /app/log_service
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py runserver 0.0.0.0:8000 &
+
+# Container'ı çalışır durumda tutmak için
+tail -f /dev/null
