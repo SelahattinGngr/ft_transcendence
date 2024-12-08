5007e13ae43a49dd25edf3fca7ec47ce4081d212
Beste <bestegursoy52@hotmail.com>
2024-12-08 14:10:25 +0300

    log_service fix

---
+++ project/auth_service/src/addLog.py
@@ -0,0 +1,24 @@
+import logging
+import os
+import requests
+
+logger = logging.getLogger(__name__)
+
+class Log:
+    def add_log(service_name, log_message, request):
+        log_service_url = os.environ.get("LOG_SERVICE_URL")
+        log_api_url = f"{log_service_url}/log/add-log/"
+        log_data = {
+            "service_name": service_name,
+            "log_message": log_message,
+            "ip_address": Log.get_client_ip(request),
+        }
+        requests.post(log_api_url, json=log_data)
+
+    def get_client_ip(request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
--- project/log_service/src/views.py
+++ project/log_service/src/views.py
@@ -1,17 +1,26 @@
 from urllib import request
 from .models import Saved_log
+from django.http import JsonResponse
 
-class logrequest:
+class LogRequest:
     def __init__(self, service_name, log):
         self.service_name = service_name
         self.log = log
 
 def save_log(logrequest):
     if request.method == 'POST':
+        data = request.POST
+        logrequest = LogRequest(
+            service_name=data.get('service_name'),
+            log=data.get('log')
+        )
         Saved_log.objects.create(
             service_name=logrequest.service_name,
             log=logrequest.log
         )
+        return JsonResponse({'message': 'Log saved successfully'}, status=201)
+    return JsonResponse({'error': 'Invalid method'}, status=405)
+
 
 def get_logs():
     logs = Saved_log.objects.all()
